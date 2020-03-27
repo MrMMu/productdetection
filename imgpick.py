f909c08432625f82121a236ffaa00828121bdfec
Mu Mu <mumu.mm@gmail.com>
2020-03-27 12:07:05 +0000

    Update imgpick.py
    combined some functions and add a new one to deal with last image.

--- imgpick.py
+++ imgpick.py
@@ -1,15 +1,29 @@
 import wx,os
 
+class Pop(wx.Frame):
+
+    def __init__(self, *args, **kwargs):
+        super(Pop, self).__init__(*args, **kwargs)
+
+        self.InitUI()
+
+    def InitUI(self):
+
+        wx.CallLater(100, self.ShowMessage)
+
+        self.SetSize((300, 200))
+        self.SetTitle('Message box')
+        self.Centre()
+
+    def ShowMessage(self):
+        wx.MessageBox('All Done!', 'Info',
+            wx.OK | wx.ICON_INFORMATION)
+
 class TestFrame(wx.Frame):
     def __init__(self, *args, **kwargs):
         wx.Frame.__init__(self, *args, **kwargs)
-
         
-        self.Bind(wx.EVT_CHAR, self.OnKeyChar)
-        
-
         self.MaxImageSize = 500
-        
         
         #b = wx.Button(self, -1, "SELECT DIRECTORY")
         #b.Bind(wx.EVT_BUTTON, self.button_browse_path_click)
@@ -27,13 +41,14 @@ class TestFrame(wx.Frame):
         # there needs to be an "Images" directory with one or more jpegs in it in the
         # current working directory for this to work
         self.jpgs = GetJpgList(self.Path) # get all the jpegs in the Images directory
+        print(self.jpgs)
         self.CurrentJpg = 0        
         
         # starting with an EmptyBitmap, the real one will get put there
         # by the call to .DisplayNext()
         self.Image = wx.StaticBitmap(self, bitmap=wx.EmptyBitmap(self.MaxImageSize, self.MaxImageSize))
         
-        self.DisplayNextKeep()
+        self.DisplayNext(event=None,action="keep")
 
         # Using a Sizer to handle the layout: I never use absolute positioning
         box = wx.BoxSizer(wx.VERTICAL)
@@ -46,21 +61,30 @@ class TestFrame(wx.Frame):
 
         self.SetSizerAndFit(box)
         
-        wx.EVT_CLOSE(self, self.OnCloseWindow)
-        
-    def OnKeyChar(self, event):
-        if event.KeyCode == 316:  #Right key
-            print("KEEP")
-            self.frame.DisplayNextKeep()
-        elif event.KeyCode == 314:  #Left key
-            print("SKIP")
-        #print(event.KeyCode)
-        event.Skip()            
+        wx.EVT_CLOSE(self, self.OnCloseWindow)          
 
-    def DisplayNextKeep(self, event=None):
+    def DisplayNext(self, event=None,action="none"):
         # load the image
+        
+        if self.CurrentJpg==-99:
+            print("Last one or only one")
+            if action=="keep":
+                newfilename=self.KeepPath+self.jpgs[-1].split("/")[-1]
+            elif action=="skip":
+                newfilename=self.SkipPath+self.jpgs[-1].split("/")[-1]
+            else:
+                exit()
+            print(newfilename)
+            os.rename(self.jpgs[-1],newfilename.replace("jpeg","jpg"))
+            ex = Pop(None)
+            #ex.ShowMessage()       
+            #exit()
+            
+            
+        print(self.CurrentJpg)
+        print(self.jpgs[self.CurrentJpg])
         Img = wx.Image(self.jpgs[self.CurrentJpg], wx.BITMAP_TYPE_ANY)
-
+        
         # scale the image, preserving the aspect ratio
         W = Img.GetWidth()
         H = Img.GetHeight()
@@ -82,46 +106,19 @@ class TestFrame(wx.Frame):
         
         if self.CurrentJpg>0:
             print(self.jpgs[self.CurrentJpg-1])
-            newfilename=self.KeepPath+self.jpgs[self.CurrentJpg-1].split("/")[-1]
+            if action=="keep":
+                newfilename=self.KeepPath+self.jpgs[self.CurrentJpg-1].split("/")[-1]
+            elif action=="skip":
+                newfilename=self.SkipPath+self.jpgs[self.CurrentJpg-1].split("/")[-1]
+            else:
+                exit()
             print(newfilename)
-            os.rename(self.jpgs[self.CurrentJpg-1],newfilename)
+            print(self.CurrentJpg)
+            os.rename(self.jpgs[self.CurrentJpg-1],newfilename.replace("jpeg","jpg"))
 
         self.CurrentJpg += 1
         if self.CurrentJpg > len(self.jpgs) -1:
-            self.CurrentJpg = 0
-            
-    def DisplayNextSkip(self, event=None): 
-        # load the image
-        Img = wx.Image(self.jpgs[self.CurrentJpg], wx.BITMAP_TYPE_ANY)
-
-        # scale the image, preserving the aspect ratio
-        W = Img.GetWidth()
-        H = Img.GetHeight()
-        if W > H:
-            NewW = self.MaxImageSize
-            NewH = self.MaxImageSize * H / W
-        else:
-            NewH = self.MaxImageSize
-            NewW = self.MaxImageSize * W / H
-        Img = Img.Scale(NewW,NewH)
- 
-        # convert it to a wx.Bitmap, and put it on the wx.StaticBitmap
-        self.Image.SetBitmap(wx.BitmapFromImage(Img))
-
-        # You can fit the frame to the image, if you want.
-        self.Fit()
-        self.Layout()
-        self.Refresh()
-        
-        if self.CurrentJpg>0:
-            print(self.jpgs[self.CurrentJpg-1])
-            newfilename=self.SkipPath+self.jpgs[self.CurrentJpg-1].split("/")[-1]
-            print(newfilename)
-            os.rename(self.jpgs[self.CurrentJpg-1],newfilename)
-            
-        self.CurrentJpg += 1
-        if self.CurrentJpg > len(self.jpgs) -1:
-            self.CurrentJpg = 0        
+            self.CurrentJpg = -99    
 
     def OnCloseWindow(self, event):
         self.Destroy()
@@ -148,7 +145,7 @@ class TestFrame(wx.Frame):
         
         
 def GetJpgList(dir):
-    jpgs = [f for f in os.listdir(dir) if f[-4:] == ".jpg"]
+    jpgs = [f for f in os.listdir(dir) if (f[-4:] == ".jpg" or f[-5:] == ".jpeg")]
     # print "JPGS are:", jpgs
     return [os.path.join(dir, f) for f in jpgs]
 
@@ -158,7 +155,6 @@ class App(wx.App):
         self.frame = TestFrame(None, -1, "LEFT to skip, RIGHT to keep!", wx.DefaultPosition,(550,200))
         self.panel = wx.Panel(self.frame)
         
-        #self.panel.Bind(wx.EVT_KEY_UP, self.OnKeyDown)
         self.panel.Bind(wx.EVT_CHAR, self.OnKeyChar)
         
         self.SetTopWindow(self.frame)
@@ -168,10 +164,10 @@ class App(wx.App):
     def OnKeyChar(self, event):
         if event.KeyCode == 316:  #Right key
             print("KEEP")
-            self.frame.DisplayNextKeep()
+            self.frame.DisplayNext(event=None,action="keep")
         elif event.KeyCode == 314:  #Left key
             print("SKIP")
-            self.frame.DisplayNextSkip()
+            self.frame.DisplayNext(event=None,action="skip")
         #print(event.KeyCode)
         event.Skip()    
 
